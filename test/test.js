a0d04a169c9d59811e2ffefef4fb48e8dcd83af2
Stephen Young <StephenJayYoung@gmail.com>
2014-09-20 22:27:09 -0700

    fixed some palatial typos

--- test/test.js
+++ test/test.js
@@ -8,10 +8,19 @@ describe("runARace()", function() {
 	//accesses the objects
 	it('tells us who won the footrace', function() {
 	//adds the first and last name
+	});
 	var racers = {
 	'TheWinner': 'The Fastest Footracer',
 	};
-	expect(whowontherace(racers)).to.eql('The Fastest Footracer');
+	expect(whowontherace(racers.TheWinner)).to.eql('The Fastest Footracer');
+	});
+
+	// describe ('isThereANumberInThatSpace()'function() {
+	// it('verifies that there is a number in the given coordinate', function() {
+	// 	var puzzle = new puzzle (puzzlestring);
+	// 	var coordinate = {row 0, col 0};
+	// 	expect(puzzle.isThereANumberInThatSpace(coordinate).to.eql(true);
+	// });
 
 
 
