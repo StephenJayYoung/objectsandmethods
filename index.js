e9cabce161dd69f47ab21cc944c53cc75edb3f9a
Stephen Young <StephenJayYoung@gmail.com>
2014-09-20 21:20:28 -0700

    started working on the 1st test regalia. That's palatial.

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 
 	var person = {
   name: 'The Fastest Footracer',
-  age: 28
-};
+  age: 28,
+	};
 
-var race = {
-  capacity: 1600,
-  speed: {
-    max: 30,
-    min: 10
-  },
-};
+	var person2 = {
+		name: 'The Second Fastest Footracer'
+		age: 22,
+	};
 
+	var person3 = {
+		name: 'The Underdog'
+		age: 20,
+	};
+
+	var race = {
+  	capacity: 1600,
+  	speed: {
+    	max: 30,
+    	min: 10,
+  	},
+	};
+
+/**
+ * Run a race!
+ * @param {object} the race set to run.
+ * @param {object} settings - Pace at which to run
+ * @param {function} cb - Called when each 400 meter lap is run. 4 total laps will be run
+ */
--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
-var chai = ('chai epect);
+var chai = require("chai");
+var lib = require('../index');
+var runARace = lib.runARace;
 
 
-var person = {
+describe('Run a race()", function() {
+	it('tells us who wins the race that was run', function() {
+
+	};
+	expect(runARace(persons)).to.eql("The Winner is The Fastest Footracer")
